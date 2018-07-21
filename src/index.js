655d1a01c8233268e12f7d0182a68e763ff162a0
Łukasz Makuch <zopsesen@gmail.com>
2018-07-21 19:21:02 +0200

    New utility: extendArrows

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export const extractParent = fullNodeName => {
+  const lastSeparator = fullNodeName.lastIndexOf(':');
+  if (lastSeparator === -1) return null;
+  return fullNodeName.substr(0, lastSeparator);
+};
+
+export const extendArrows = arrows => {
+  return arrows.map(arrow => {
+    if (arrow.length < 1) return arrow;
+    const lastArrowPart = arrow[arrow.length - 1];
+    const lastArrowPartParent = extractParent(lastArrowPart[0]);
+    if (!lastArrowPartParent) return arrow;
+    return [
+      ...arrow,
+      [lastArrowPartParent, lastArrowPart[1]]
+    ];
+  });
+};
