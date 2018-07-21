2c35a774fc6c43ffaccfb06f87013706e9e17e38
Łukasz Makuch <zopsesen@gmail.com>
2018-07-21 19:28:47 +0200

    Exported utility: extractParent

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// Extracting the parent node:
 export const extractParent = fullNodeName => {
   const lastSeparator = fullNodeName.lastIndexOf(':');
   if (lastSeparator === -1) return null;
   return fullNodeName.substr(0, lastSeparator);
 };
 
+// Extending arrows:
 export const extendArrows = arrows => {
   return arrows.map(arrow => {
     if (arrow.length < 1) return arrow;
--- src/module.test.js
+++ src/module.test.js
@@ -1,6 +1,18 @@
-import {extendArrows} from '.';
+import {extendArrows, extractParent} from '.';
 
 describe('Rosmaro binding utils', () => {
+
+  describe('extracting the parent node', () => {
+
+    it('returns null if the node has no parent', () => {
+      expect(extractParent('a')).toEqual(null);
+    });
+
+    it('returns the parent node if the is any', () => {
+      expect(extractParent('a:b:c')).toEqual('a:b');
+    });
+
+  });
 
   describe('extending arrows', () => {
 
